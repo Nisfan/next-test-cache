815df7b3e7762e3a1ee5d89c4bc9c7d990d4e1d6
Nisfan <mohnisfan@gmail.com>
2023-09-03 16:25:44 +0530

    testing cache with form action

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Footer from "./Footer";
 
 export default function Home() {
-  return <main className={styles.main}>{Date.now()}</main>;
+  return (
+    <main className={styles.main}>
+      {Date.now()}
+      <Footer />
+    </main>
+  );
 }
